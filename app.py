1386dba4f7ff9aa8075499394b383bc9eefb8c56
toprateddigitalagency-dotcom <toprated.digitalagency@gmail.com>
2025-07-31 11:44:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ import os
 from telegram.ext import Application, CommandHandler
 from commands.help import help_command
 from commands.balance import balance_command
+from commands.addorder import addorder_command
+from commands.orders import orders_command
+from commands.profit import profit_command
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    # Регистрируем команды
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("balance", balance_command))
+    app.add_handler(CommandHandler("addorder", addorder_command))
+    app.add_handler(CommandHandler("orders", orders_command))
+    app.add_handler(CommandHandler("profit", profit_command))
 
     print("НАФАНЯ запущен...")
     app.run_polling()
